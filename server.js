752fd8970eb6b691dad30ed134969bdb5c450505
Cliff Saporta Cheng <ccfcheng@gmail.com>
2016-02-02 11:22:04 -0800

    feat(dropbox): Connect to Dropbox and convert Excel to JSON array

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var path = require('path');
 // ==============================================
 // sample route with a route the way we're used to seeing it
 app.get('/sample', function(req, res) {
-  res.send('this is a sample!');  
+  res.send('this is a sample!');
 });
 
 // we'll create our routes here
@@ -34,31 +34,144 @@ console.log('Magic happens on port ' + port);
 
 // JS-XSLX Setup
 var XLSX = require('xlsx');
-// URL for test file hosted on dropbox
-// var url = 'https://www.dropbox.com/s/jmm55r6fjiqd1yp/test_data.xlsx?dl=1';
-// var https = require('https');
-
-// https.get(url, function(res) {
-
-//   var data = [];
-  
-//   res.on('data', function(chunk) {
-//     data.push(chunk);
-//   });
-
-//   res.on('end', function() {
-//     data = Buffer.concat(data);
-//     console.log(data.toString());
-//   });
-
-// });
 
 var workbook = XLSX.readFile('./public/assets/test_data.xlsx');
 // console.log(workbook.SheetNames);
 // This is the reference to the inventory sheet
 var worksheet = workbook.Sheets[workbook.SheetNames[3]];
-var json_data = XLSX.utils.sheet_to_json(worksheet);
+// var json_data = XLSX.utils.sheet_to_json(worksheet);
 // console.log('In JSON:', json_data);
-for (var cell in worksheet) {
-  console.log(cell + ':', JSON.stringify(worksheet[cell].v));
-}
+// var json_data = {};
+
+// Grabs headers from an excel worksheet
+var getHeaders = function(worksheet) {
+  var headers = [];
+  for (var cell in worksheet) {
+    if (cell.substr(1) === '1') {
+      headers.push(worksheet[cell].v);
+    }
+  }
+  return headers;
+};
+
+// Splits a worksheet into rows
+var splitRows = function(worksheet) {
+  var result = [];
+  var row = [];
+  for (var cell in worksheet) {
+    if (cell[0] === 'A' && row.length !== 0) {
+      // We're on a new row, reset row array and start pushing
+      result.push(row);
+      row = [];
+    }
+    row.push(worksheet[cell].v);
+  }
+  return result;
+};
+
+// Makes a default row object
+var makeRow = function(worksheet) {
+  var obj = {};
+  var headers = getHeaders(worksheet);
+  headers.forEach(function(header) {
+    obj[header] = '';
+  });
+  return obj;
+};
+
+// Converts worksheet into JSON
+var convertJSON = function(worksheet) {
+  var row = makeRow(worksheet);
+  var headers = getHeaders(worksheet);
+  var json_array = [];
+  for (var cell in worksheet) {
+    switch (cell[0]) {
+      case "A":
+        row[headers[0]] = worksheet[cell].v;
+        break;
+      case "B":
+        row[headers[1]] = worksheet[cell].v;
+        break;
+      case "C":
+        row[headers[2]] = worksheet[cell].v;
+        break;
+      case "D":
+        row[headers[3]] = worksheet[cell].v;
+        break;
+      case "E":
+        row[headers[4]] = worksheet[cell].v;
+        break;
+      case "F":
+        row[headers[5]] = worksheet[cell].v;
+        break;
+      case "G":
+        row[headers[6]] = worksheet[cell].v;
+        break;
+      case "H":
+        row[headers[7]] = worksheet[cell].v;
+        break;
+      case "I":
+        row[headers[8]] = worksheet[cell].v;
+        break;
+      case "J":
+        row[headers[9]] = worksheet[cell].v;
+        break;
+      case "K":
+        row[headers[10]] = worksheet[cell].v;
+        break;
+      case "L":
+        row[headers[11]] = worksheet[cell].v;
+        break;
+      case "M":
+        row[headers[12]] = worksheet[cell].v;
+        break;
+      case "N":
+        row[headers[13]] = worksheet[cell].v;
+        break;
+      case "O":
+        row[headers[14]] = worksheet[cell].v;
+        break;
+      case "P":
+        row[headers[15]] = worksheet[cell].v;
+        break;
+      case "Q":
+        row[headers[16]] = worksheet[cell].v;
+        break;
+      case "R":
+        row[headers[17]] = worksheet[cell].v;
+        break;
+      case "S":
+        row[headers[18]] = worksheet[cell].v;
+        break;
+      case "T":
+        row[headers[19]] = worksheet[cell].v;
+        break;
+      case "U":
+        row[headers[20]] = worksheet[cell].v;
+        break;
+      case "V":
+        row[headers[21]] = worksheet[cell].v;
+        break;
+      case "W":
+        row[headers[22]] = worksheet[cell].v;
+        json_array.push(row);
+        row = makeRow(worksheet);
+        break;
+      default:
+        break;
+    }
+  }
+  return json_array;
+};
+
+console.log(convertJSON(worksheet));
+// console.log('Headers:', getHeaders(worksheet));
+// console.log('Rows:', splitRows(worksheet));
+// var cells = [];
+// for (var cell in worksheet) {
+//   cells.push(cell.substr(1));
+// }
+// console.log(cells.join(', '));
+// for (var cell in worksheet) {
+//   console.log(cell + ':', worksheet[cell].v);
+// }
