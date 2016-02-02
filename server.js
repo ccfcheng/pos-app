cb6af2b8c075467c3266a6297fe5eb694ad7dc31
Cliff Saporta Cheng <ccfcheng@gmail.com>
2016-02-01 23:36:57 -0800

    feat(XLSX Parser): Add ability to parse excel file locally
    XLSX works well for local files
    Can't seem to correctly get access to Dropbox files, will see if other cloud options work

--- server.js
+++ server.js
@@ -31,3 +31,34 @@ app.use('/', router);
 // ==============================================
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+// JS-XSLX Setup
+var XLSX = require('xlsx');
+// URL for test file hosted on dropbox
+// var url = 'https://www.dropbox.com/s/jmm55r6fjiqd1yp/test_data.xlsx?dl=1';
+// var https = require('https');
+
+// https.get(url, function(res) {
+
+//   var data = [];
+  
+//   res.on('data', function(chunk) {
+//     data.push(chunk);
+//   });
+
+//   res.on('end', function() {
+//     data = Buffer.concat(data);
+//     console.log(data.toString());
+//   });
+
+// });
+
+var workbook = XLSX.readFile('./public/assets/test_data.xlsx');
+// console.log(workbook.SheetNames);
+// This is the reference to the inventory sheet
+var worksheet = workbook.Sheets[workbook.SheetNames[3]];
+var json_data = XLSX.utils.sheet_to_json(worksheet);
+// console.log('In JSON:', json_data);
+for (var cell in worksheet) {
+  console.log(cell + ':', JSON.stringify(worksheet[cell].v));
+}
